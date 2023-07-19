a73a8bb36183c4d4ada41e4f0d5e68d24557a8ac
JuanDNJ <juandevnjv@gmail.com>
2023-07-19 07:07:10 +0200

    REFACT: Componente login y logout

--- src/components/Auth/Login/index.jsx
+++ src/components/Auth/Login/index.jsx
@@ -2,29 +2,26 @@ import './login.css'
 import {useAuthContext} from '../../../contexts/authContext';
 import { Link } from 'react-router-dom';
 import { Navigate} from 'react-router-dom';
-import {useState, useEffect} from 'react';
-import { MAGIC_WORD } from '../../../consts';
 import { useAuthLogin } from '../../../api/api-mysql';
 const Login = () => {
   const {login, isAuthenticated} = useAuthContext();
-  const [magicWord, setMagicWord] = useState('');
-  
+ 
   if(isAuthenticated){
     return <Navigate to="/" />;
   }
 
-  const handleInputChange = (e) => setMagicWord(e.target.value);
   const handlerSubmit = (eve) => {
     const {email, password} = eve.target;
     eve.preventDefault();
-    // if(magicWord === MAGIC_WORD){
-    //   login();
-    // }
+   
     useAuthLogin(email.value, password.value)
     .then((res) => {
       console.log(res);
-      if(res.user.is_active === 1){
-        login();  
+      if(res.status !== 200){
+        alert(res.message);
+      }
+      if(res.user && res.user.is_active === 1){
+        login((res.token));  
       }
       
     })
@@ -37,7 +34,7 @@ const Login = () => {
       <h1>Login</h1>
       <form onSubmit={handlerSubmit}>
         <input type="email" name="email" id="email" placeholder='Insert your email address' />
-        <input type="password" name="password" id="password" placeholder='Insert your password' />
+        <input type="password" name="password" id="password" placeholder='Insert your password' autoComplete="off" />
         <button type="submit">Sign In</button>
       </form>
       <Link className="new-account" to="/new-account">You do not have an account?</Link>
