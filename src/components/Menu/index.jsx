dc2b5c46424694bace9a89a6ef0b5ab8c931f6f7
JuanDNJ <juandevnjv@gmail.com>
2023-07-27 10:34:17 +0200

    REFACT: resize window

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -4,40 +4,54 @@ import { useStore } from '../../contexts/storeContext'
 import { Link } from 'react-router-dom'
 import { createPortal } from 'react-dom'
 import Userwidtget from '../User/UserWidget'
+import { useEffect, useState } from 'react'
 
 export const MenuBars = () => {
   const { toggleMenuBars } = useStore()
   return (
-        <section className={styles.menuBars}>
-            <div className={styles.bars} onClick={() => toggleMenuBars()}>
-                <span className={styles.bar}></span>
-                <span className={styles.bar}></span>
-                <span className={styles.bar}></span>
-            </div>
-        </section>
+    <section className={styles.menuBars}>
+      <div className={styles.bars} onClick={() => toggleMenuBars()}>
+        <span className={styles.bar}></span>
+        <span className={styles.bar}></span>
+        <span className={styles.bar}></span>
+      </div>
+    </section>
   )
 }
 
 export const Menu = ({ isMobile }) => {
   const { isAuthenticated } = useAuthContext()
-
+  const { isOpenMenuBars, closeMenuBars } = useStore(false)
   return (
-        <section className={`${styles.menu} ${isMobile ? styles.menuMobile : styles.descktop}`}>
-            <Link to="/" >Inicio</Link>
-            <Link to="/about" >Quienes somos?</Link>
-            <Link to="/mascotas" >Mascotas</Link>
-            <Link to="/contact" >Contacto</Link>
-            {!isAuthenticated ? <Link to="/login" >Sign In</Link> : <Userwidtget />}
-        </section>)
+    <section className={`${styles.menu} ${isMobile ? styles.menuMobile : styles.descktop}`} onClick={closeMenuBars}>
+      <Link to="/" >Inicio</Link>
+      <Link to="/about" >Quienes somos?</Link>
+      <Link to="/mascotas" >Mascotas</Link>
+      <Link to="/contact" >Contacto</Link>
+      {!isAuthenticated ? <Link to="/login" >Sign In</Link> : <Userwidtget />}
+    </section>)
 }
 
 const Nav = () => {
-  const { isOpenMenuBars } = useStore(false)
+  const { isOpenMenuBars, closeMenuBars } = useStore(false)
+  useEffect(() => {
+    closeMenuBars()
+    window.addEventListener('resize', closeMenuBars)
+    return () => window.removeEventListener('resize', closeMenuBars)
+  }, [])
   return (
-        <nav className={styles.navigation} >
-            {(isOpenMenuBars && createPortal(<Menu isMobile={isOpenMenuBars} />, document.body, 'menu')) || (<Menu isDescktop={true} />)}
-            <MenuBars />
-        </nav>
+    <nav className={styles.navigation} >
+      {
+        (
+          isOpenMenuBars
+          && createPortal(<Menu isMobile={isOpenMenuBars} />, document.body, 'menu')
+        )
+        || (
+          <Menu isMobile={false} />
+        )
+      }
+      <MenuBars />
+    </nav>
   )
 }
 export default Nav
