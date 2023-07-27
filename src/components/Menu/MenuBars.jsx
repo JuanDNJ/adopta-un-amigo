3878d833594c933aa9c731649c37163c9ce1b9aa
JuanDNJ <juandevnjv@gmail.com>
2023-07-27 22:58:22 +0200

    ADD: close onClick Userwidtget

--- src/components/Menu/MenuBars.jsx
+++ src/components/Menu/MenuBars.jsx
@@ -1,11 +1,12 @@
 import styles from './menu.module.css'
 import { useStore } from '../../contexts/storeContext'
-
+import { useMenuContext } from '../../contexts/menuContext'
 export const MenuBars = () => {
   
     const { toggleMenuBars } = useStore()
+    const { close } = useMenuContext()
     return (
-      <section className={styles.menuBars}>
+      <section className={styles.menuBars} onClick={() => close()}>
         <div className={styles.bars} onClick={() => toggleMenuBars()}>
           <span className={styles.bar}></span>
           <span className={styles.bar}></span>
